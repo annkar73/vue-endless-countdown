941af7b677368203de9ef7be9eab13c54dbe9ac6
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2024-08-14 19:36:15 +0200

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: "/vue-endless-counter/",
+  base: "/vue-endless-countdown/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
